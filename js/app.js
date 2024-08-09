de94876bb6c772808c6b604af630d414477fae49
samtasticc <samantha.henesy@gmail.com>
2024-08-08 20:09:09 -0400

    add headers, buttons, and css styling

---
+++ js/app.js
@@ -0,0 +1,19 @@
+/*-------------- Constants -------------*/
+// !const handleMov = (event) => {
+     //!logic for the event handler, update state variables, etc.
+
+     //!Render data to the DOM
+//!     render()
+// !}
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
+
